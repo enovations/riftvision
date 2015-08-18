510ba9aa6701a0fa41534af303bb86dc0a1e4998
Pylo <klemen.pylo@gmail.com>
2015-08-18 16:55:37 +0200

    update

--- Sensors/mami.py
+++ Sensors/mami.py
@@ -20,27 +20,25 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-loop = 1000 / 20
+interval = 1000 / 20
 updates = 0
 delta = 0
+start_time = int(round(time.time() * 1000))
+timer = int(round(time.time() * 1000))
+
+updates = 0
 
 while 1:
-  start_time = int(round(time.time() * 1000))
-  ss = ovrHmd_GetSensorState(hmd, ovr_GetTimeInSeconds())
-  pose = ss.Predicted.Pose
-  MESSAGE = str(pose.Orientation.w) + ';' + str(pose.Orientation.x) + ';' + str(pose.Orientation.y) + ';' + str(pose.Orientation.z) + '\n\r'
-  sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-  #print(MESSAGE)
-  time.sleep(0.016)
-  #time.sleep(0.1)
-  end_time = int(round(time.time()*1000))
-  difference = end_time - start_time
-  if difference > 0:
-    sleep((loop - difference) / 1000.0)
-    updates = updates + 1
-    delta = delta + difference
-    if updates == 20:
-      updates = 0
+  now = int(round(time.time() * 1000)) - start_time
+  delta += now - start_time
+  start_time = now
+
+  while delta >= interval:
+    ss = ovrHmd_GetSensorState(hmd, ovr_GetTimeInSeconds())
+    pose = ss.Predicted.Pose
+    MESSAGE = str(pose.Orientation.w) + ';' + str(pose.Orientation.x) + ';' + str(pose.Orientation.y) + ';' + str(pose.Orientation.z) + '\n\r'
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    delta -= interval
 
 vrHmd_Destroy(hmd)
 ovr_Shutdown()
