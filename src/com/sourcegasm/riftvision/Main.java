19868d46bc45a30d41b6e6d7be2f54a2cf9e4333
Pylo <klemen.pylo@gmail.com>
2015-08-19 10:02:26 +0200

    FIx

--- src/com/sourcegasm/riftvision/Main.java
+++ src/com/sourcegasm/riftvision/Main.java
@@ -1,13 +1,17 @@
 package com.sourcegasm.riftvision;
 
 import com.sourcegasm.riftvision.control.DroneController;
+import com.sourcegasm.riftvision.render.RenderFrame;
+import com.sourcegasm.riftvision.render.RenderManager;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         //OculusSensors.startReceiving();
+    	RenderFrame frame = new RenderFrame();
         DroneController controller = new DroneController();
+        RenderManager manager = new RenderManager(controller.getDrone(), frame);
         //controller.
     }
 }
