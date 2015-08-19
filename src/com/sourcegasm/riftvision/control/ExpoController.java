cee564a53f3b0ba526c94fc5b5fadc3f7d5544b3
Pylo <klemen.pylo@gmail.com>
2015-08-19 15:00:42 +0200

    added ExpoController

---
+++ src/com/sourcegasm/riftvision/control/ExpoController.java
@@ -0,0 +1,18 @@
+package com.sourcegasm.riftvision.control;
+
+/**
+ * Created by ziga on 21.8.2014.
+ */
+
+
+public class ExpoController {
+    public static double getExpo(double power) {
+        double result;
+        if (power < 0) result = -(10 * Math.exp(-0.035 * power) - 10);
+        else result = 10 * Math.exp(0.035 * power) - 10;
+        if(result>20)return 20;
+        if(result<-20)return -20;
+        return result;
+    }
+
+}
