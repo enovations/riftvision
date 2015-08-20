17bb3c5e0fd03782094ef3c5250795e335cdf6a1
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-08-20 20:37:05 +0200

    toString implenented

--- src/com/sourcegasm/riftvision/render/RenderManager.java
+++ src/com/sourcegasm/riftvision/render/RenderManager.java
@@ -8,14 +8,14 @@ import java.awt.image.RescaleOp;
 
 public class RenderManager {
 	public RenderManager(DroneController controller, final RenderRiftWindow frame) {
-		/*controller.getDrone().addImageListener(new BufferedImageVideoListener() {
+		controller.getDrone().addImageListener(new BufferedImageVideoListener() {
 			@Override
 			public void imageReceived(BufferedImage image) {
 				RescaleOp rescaleOp = new RescaleOp(1.2f, 15, null);
 				rescaleOp.filter(image, image);
 				frame.updateFrame(image, controller.getNavData());
 			}
-		});*/
+		});
 	}
 
 }
