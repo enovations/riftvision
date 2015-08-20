217d8fb47ee4536ef375a673f746d222591f20f6
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-08-20 08:34:00 +0200

    merge

--- src/com/sourcegasm/riftvision/control/HeightController.java
+++ src/com/sourcegasm/riftvision/control/HeightController.java
@@ -4,27 +4,26 @@ package com.sourcegasm.riftvision.control;
  * Created by klemen on 19.8.2015.
  */
 public class HeightController {
-	private int up = 0;
+    private int up = 0;
 
-	public double getHeightMove() {
-		double result = up / 100.0;
-		if (up > 0)
-			--up;
-		else if (up < 0)
-			++up;
-		//TODO: max values, up/down
-		return result;
-	}
+    public double getHeightMove() {
+        double result = up / 100.0;
+        if (up > 0)
+            --up;
+        else if (up < 0)
+            ++up;
+        return result;
+    }
 
-	public void reset(){
-		up = 0;
-	}
+    public void increaseHeightMove() {
+        if (up < 100) up += 20;
+    }
 
-	public void increaseHeightMove() {
-		up += 20;
-	}
+    public void decreaseHeightMove() {
+        if (up > -100) up -= 20;
+    }
 
-	public void decreaseHeightMove() {
-		up -= 20;
-	}
+    public void reset() {
+        up = 0;
+    }
 }
